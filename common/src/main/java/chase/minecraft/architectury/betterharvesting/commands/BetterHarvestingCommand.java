46e71bd6e043fb6f4cdb528c59757f1d839ea529
Drew Chase <dcmanproductions@gmail.com>
2023-04-29 21:15:40 -0400

    Config is now dynamic

--- common/src/main/java/chase/minecraft/architectury/betterharvesting/commands/BetterHarvestingCommand.java
+++ common/src/main/java/chase/minecraft/architectury/betterharvesting/commands/BetterHarvestingCommand.java
@@ -1,106 +1,89 @@
 package chase.minecraft.architectury.betterharvesting.commands;
 
-import chase.minecraft.architectury.betterharvesting.config.BetterHarvestingConfig;
 import chase.minecraft.architectury.betterharvesting.config.ConfigHandler;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
+
+import java.util.Objects;
 
 import static com.mojang.brigadier.arguments.BoolArgumentType.bool;
 import static com.mojang.brigadier.arguments.BoolArgumentType.getBool;
+import static com.mojang.brigadier.arguments.DoubleArgumentType.doubleArg;
+import static com.mojang.brigadier.arguments.DoubleArgumentType.getDouble;
+import static com.mojang.brigadier.arguments.FloatArgumentType.floatArg;
+import static com.mojang.brigadier.arguments.FloatArgumentType.getFloat;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
 import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
 
 public class BetterHarvestingCommand
 {
 	
+	
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher)
 	{
-		dispatcher.register(
-				literal("betterharvesting")
-						.requires((commandSourceStack) -> commandSourceStack.hasPermission(4))
+		LiteralArgumentBuilder<CommandSourceStack> cmd = literal("betterharvesting")
+				.requires((commandSourceStack) -> commandSourceStack.hasPermission(4))
+				.executes(context ->
+				{
+					context.getSource().sendSuccess(ConfigHandler.getInstance().getAll(), true);
+					return 1;
+				})
+				.then(literal("reload")
 						.executes(context ->
 						{
-							MutableComponent component = Component.literal("Better Harvesting Configuration:\n").withStyle(ChatFormatting.AQUA);
-							component.append(Component.literal("%sAllow Fast Leaf Decay%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowFastLeafDecay ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowFastLeafDecay ? "ENABLED" : "DISABLED")));
-							component.append(Component.literal("%sAllow Right Click Harvest%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowRightClickHarvest ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowRightClickHarvest ? "ENABLED" : "DISABLED")));
-							component.append(Component.literal("%sAllow Automatic Sapling Replant%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowAutomaticSaplingReplant ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowAutomaticSaplingReplant ? "ENABLED" : "DISABLED")));
-							context.getSource().sendSuccess(component, true);
+							ConfigHandler.getInstance().load();
+							context.getSource().sendSuccess(Component.literal("[Better Harvesting] reloaded config"), true);
 							return 1;
-						})
-						.then(literal("reload")
-								.executes(context ->
-								{
-									ConfigHandler.getInstance().load();
-									context.getSource().sendSuccess(Component.literal("[Better Harvesting] reloaded config"), true);
-									return 1;
-								}))
-						.then(
-								literal("fast_leaf_decay")
-										.executes(context ->
-										{
-											context.getSource().sendSuccess(Component.literal("%sAllow Fast Leaf Decay%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowFastLeafDecay ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowFastLeafDecay ? "ENABLED" : "DISABLED")), true);
-											ConfigHandler.getInstance().load();
-											return 1;
-										})
-										.then(
-												argument("value", bool())
-														.executes(context ->
-														{
-															BetterHarvestingConfig config = ConfigHandler.getInstance().config;
-															config.AllowFastLeafDecay = getBool(context, "value");
-															ConfigHandler.getInstance().config = config;
-															context.getSource().sendSuccess(Component.literal("%sAllow Fast Leaf Decay%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowFastLeafDecay ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowFastLeafDecay ? "ENABLED" : "DISABLED")), true);
-															ConfigHandler.getInstance().save();
-															return 1;
-														})
-										)
-						)
-						.then(
-								literal("right_click_harvest")
-										.executes(context ->
-										{
-											context.getSource().sendSuccess(Component.literal("%sAllow Right Click Harvest%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowRightClickHarvest ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowRightClickHarvest ? "ENABLED" : "DISABLED")), true);
-											ConfigHandler.getInstance().load();
-											return 1;
-										})
-										.then(
-												argument("value", bool())
-														.executes(context ->
-														{
-															BetterHarvestingConfig config = ConfigHandler.getInstance().config;
-															config.AllowFastLeafDecay = getBool(context, "value");
-															ConfigHandler.getInstance().config = config;
-															context.getSource().sendSuccess(Component.literal("%sAllow Right Click Harvest%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowRightClickHarvest ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowRightClickHarvest ? "ENABLED" : "DISABLED")), true);
-															ConfigHandler.getInstance().save();
-															return 1;
-														})
-										)
-						)
-						.then(
-								literal("auto_sapling_replant")
-										.executes(context ->
-										{
-											ConfigHandler.getInstance().load();
-											context.getSource().sendSuccess(Component.literal("%sAllow Automatic Sapling Replant%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowAutomaticSaplingReplant ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowAutomaticSaplingReplant ? "ENABLED" : "DISABLED")), true);
-											return 1;
-										})
-										.then(
-												argument("value", bool())
-														.executes(context ->
-														{
-															BetterHarvestingConfig config = ConfigHandler.getInstance().config;
-															config.AllowAutomaticSaplingReplant = getBool(context, "value");
-															ConfigHandler.getInstance().config = config;
-															context.getSource().sendSuccess(Component.literal("%sAllow Automatic Sapling Replant%s is %s%s\n".formatted(ChatFormatting.GOLD, ChatFormatting.RESET, ConfigHandler.getInstance().config.AllowAutomaticSaplingReplant ? ChatFormatting.GREEN : ChatFormatting.RED, ConfigHandler.getInstance().config.AllowAutomaticSaplingReplant ? "ENABLED" : "DISABLED")), true);
-															ConfigHandler.getInstance().save();
-															return 1;
-														})
-										)
-						)
-		);
+						}));
+		for (String field : ConfigHandler.getInstance().suggestions())
+		{
+			LiteralArgumentBuilder<CommandSourceStack> sub = literal(field);
+			if (ConfigHandler.getInstance().get(field) instanceof Boolean)
+			{
+				sub = sub.then(argument("value", bool()).executes(ctx -> set(ctx, field, getBool(ctx, "value")) ? 1 : 0));
+			} else if (ConfigHandler.getInstance().get(field) instanceof Integer)
+			{
+				sub = sub.then(argument("value", integer()).executes(ctx -> set(ctx, field, getInteger(ctx, "value")) ? 1 : 0));
+			} else if (ConfigHandler.getInstance().get(field) instanceof Float)
+			{
+				sub = sub.then(argument("value", floatArg()).executes(ctx -> set(ctx, field, getFloat(ctx, "value")) ? 1 : 0));
+			} else if (ConfigHandler.getInstance().get(field) instanceof Double)
+			{
+				sub = sub.then(argument("value", doubleArg()).executes(ctx -> set(ctx, field, getDouble(ctx, "value")) ? 1 : 0));
+			}
+			sub = sub.executes(ctx -> get(ctx, field) ? 1 : 0);
+			cmd.then(sub);
+		}
+		dispatcher.register(cmd);
+	}
+	
+	private static boolean set(CommandContext<CommandSourceStack> context, String name, Object value)
+	{
+		if (!ConfigHandler.getInstance().exists(name))
+		{
+			context.getSource().sendFailure(Component.literal("[Better Harvesting] Config does NOT exist: %s".formatted(name)).withStyle(ChatFormatting.RED));
+			return false;
+		}
+		ConfigHandler.getInstance().set(name, value);
+		context.getSource().sendSuccess(Objects.requireNonNull(ConfigHandler.getInstance().getAsComponent(name)), true);
+		return true;
+	}
+	
+	private static boolean get(CommandContext<CommandSourceStack> context, String name)
+	{
+		if (!ConfigHandler.getInstance().exists(name))
+		{
+			context.getSource().sendFailure(Component.literal("[Better Harvesting] Config does NOT exist: %s".formatted(name)).withStyle(ChatFormatting.RED));
+			return false;
+		}
+		context.getSource().sendSuccess(Objects.requireNonNull(ConfigHandler.getInstance().getAsComponent(name)), true);
+		return true;
 	}
 	
 }
--- common/src/main/java/chase/minecraft/architectury/betterharvesting/config/ConfigHandler.java
+++ common/src/main/java/chase/minecraft/architectury/betterharvesting/config/ConfigHandler.java
@@ -4,20 +4,27 @@ import chase.minecraft.architectury.betterharvesting.BetterHarvesting;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dev.architectury.platform.Platform;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
+import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class ConfigHandler
 {
 	private final File CONFIG_FILE = Path.of(Platform.getConfigFolder().toString(), "betterharvesting.json").toFile();
 	private static ConfigHandler instance = null;
-	public BetterHarvestingConfig config;
+	private BetterHarvestingConfig config;
 	
 	protected ConfigHandler()
 	{
 		instance = this;
 		config = new BetterHarvestingConfig();
+		
 		load();
 	}
 	
@@ -39,12 +46,10 @@ public class ConfigHandler
 			} catch (IOException ex)
 			{
 				BetterHarvesting.log.error("Unable to create config file: {}, {}", CONFIG_FILE.getAbsolutePath(), ex.getMessage());
-				save();
 			}
 		} catch (IOException e)
 		{
 			BetterHarvesting.log.error("Unable to read config file: {}, {}", CONFIG_FILE.getAbsolutePath(), e.getMessage());
-			save();
 		}
 		if (config == null)
 			save();
@@ -64,8 +69,76 @@ public class ConfigHandler
 		}
 	}
 	
+	public String[] suggestions()
+	{
+		return Arrays.stream(config.getClass().getDeclaredFields()).map(Field::getName).toArray(String[]::new);
+	}
+	
+	public void set(String name, Object value)
+	{
+		try
+		{
+			Field field = config.getClass().getDeclaredField(name);
+			field.set(config, value);
+			save();
+		} catch (NoSuchFieldException | IllegalAccessException e)
+		{
+			BetterHarvesting.log.error("Unable to set config field: '{}' to '{}', {}", name, value, e.getMessage());
+		}
+	}
+	
+	public @Nullable Object get(String name)
+	{
+		try
+		{
+			Field field = config.getClass().getDeclaredField(name);
+			return field.get(config);
+			
+		} catch (NoSuchFieldException | IllegalAccessException e)
+		{
+			BetterHarvesting.log.error("Unable to get config field: '{}' , {}", name, e.getMessage());
+		}
+		return null;
+	}
+	
+	public @Nullable Component getAsComponent(String name)
+	{
+		@Nullable Object value = get(name);
+		if (value != null)
+		{
+			if (value instanceof Boolean bool)
+			{
+				return Component.literal("%s%s%s is %s%s".formatted(ChatFormatting.GOLD, name, ChatFormatting.RESET, bool ? ChatFormatting.GREEN : ChatFormatting.RED, bool ? "ENABLED" : "DISABLED"));
+			}
+			return Component.literal("%s%s%s is %s%s".formatted(ChatFormatting.GOLD, name, ChatFormatting.RESET, ChatFormatting.GREEN, value));
+		}
+		return null;
+	}
+	
+	public boolean exists(String name)
+	{
+		return get(name) != null;
+	}
+	
+	public Component getAll()
+	{
+		
+		MutableComponent component = Component.literal("Better Harvesting Configuration:\n").withStyle(ChatFormatting.AQUA);
+		for (Component field : Arrays.stream(suggestions()).map(this::getAsComponent).toArray(Component[]::new))
+		{
+			component.append(field);
+			component.append("\n");
+		}
+		return component;
+	}
+	
 	public static ConfigHandler getInstance()
 	{
 		return instance == null ? new ConfigHandler() : instance;
+	}
+	
+	public static BetterHarvestingConfig getConfig()
+	{
+		return getInstance().config;
 	}
 }
