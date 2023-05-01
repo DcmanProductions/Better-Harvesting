cad3ecf46cae2d4aea6ee9d8d03c55eddd243308
Drew Chase <dcmanproductions@gmail.com>
2023-05-01 10:24:55 -0400

    added networking, function for mod resource location

--- common/src/main/java/chase/minecraft/architectury/betterharvesting/BetterHarvesting.java
+++ common/src/main/java/chase/minecraft/architectury/betterharvesting/BetterHarvesting.java
@@ -4,12 +4,18 @@ package chase.minecraft.architectury.betterharvesting;
 import chase.minecraft.architectury.betterharvesting.commands.BetterHarvestingCommand;
 import chase.minecraft.architectury.betterharvesting.modules.RightClickHarvestModule;
 import chase.minecraft.architectury.betterharvesting.modules.VeinMiningModule;
+import chase.minecraft.architectury.betterharvesting.networking.BetterHarvestingNetworking;
 import dev.architectury.event.events.common.CommandRegistrationEvent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashMap;
+
 public class BetterHarvesting
 {
+	public static HashMap<ServerPlayer, Boolean> isVeinmineKeyDown = new HashMap<>();
 	public static final String MOD_ID = "betterharvesting";
 	public static final Logger log = LogManager.getLogger(MOD_ID);
 	
@@ -17,6 +23,18 @@ public class BetterHarvesting
 	{
 		RightClickHarvestModule.init();
 		VeinMiningModule.init();
+		BetterHarvestingNetworking.ServerNetworking.init();
 		CommandRegistrationEvent.EVENT.register((dispatcher, context, selection) -> BetterHarvestingCommand.register(dispatcher));
+	}
+	
+	/**
+	 * Allows the creation of Resource Location with the warp namespace.
+	 *
+	 * @param name
+	 * @return
+	 */
+	public static ResourceLocation id(String name)
+	{
+		return new ResourceLocation(MOD_ID, name);
 	}
 }
